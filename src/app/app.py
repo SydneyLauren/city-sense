054e5e22dbdf47ea038ee877353edd5c172c5dbf
Sydney Decoto <sydney.decoto@gmail.com>
2016-11-26 22:59:11 -0800

    update app files

--- src/app/app.py
+++ src/app/app.py
@@ -1,90 +1,33 @@
 from __future__ import division
 from sklearn.metrics.pairwise import cosine_similarity
 from flask import Flask, render_template, request, jsonify, make_response
-from geopy.geocoders import Nominatim
-import pandas as pd
 import numpy as np
-import cStringIO
+import setup_app
 import numpy as np
-import re
-import pickle
-import matplotlib.pyplot as plt
 app = Flask(__name__)
 
 
-IMAGE_PATH = 'static/images/mapimg_{}.png'
-# load the dataframe that was built in build_model.py
-cities_df = pd.read_pickle('../../data/cities_dataframe.pkl')
-sentence_df = pd.read_pickle('../../data/sentence_dataframe.pkl')
-
-# load the vectorized you pickled in build_model.py
-with open('../../data/my_vectorizer.pkl') as f:
-    tfidf = pickle.load(f)
-with open('../../data/sentence_vectorizer.pkl') as f:
-    tfidf_sentence = pickle.load(f)
-
+# Load the dataframes containing corpus
+cities_df, sentence_df = setup_app.load_dataframes(['../../data/cities_dataframe.pkl',
+                                                    '../../data/sentence_dataframe.pkl'])
 # extract cities and their descripitions from dataframe
 doc_bodies = cities_df['description'].values
 sent_bodies = sentence_df['description'].values
-
 cities = cities_df.index.values
 sent_cities = sentence_df.index.values
 
+# load the vectorized models pickled in build_model.py
+tfidf, tfidf_sentence = setup_app.load_models(['../../data/my_vectorizer.pkl',
+                                               '../../data/sentence_vectorizer.pkl'])
+# fit the model
 X = tfidf.fit_transform(doc_bodies)
 
 # read personality words from text file
 personality_dict = dict()
 
-# load the basemap object for plotting
-with open('../../data/base_map.pkl') as f:
-    m = pickle.load(f)
-
-# load the city coordinates
-with open('../../data/coordinates.txt') as f:
-    coords = np.array([[float(coord) for coord in line.strip('\n').split('|')[1:]] for line in f])
-
-
-def plot_personality_map(m, cosine_similarities, xpts, ypts, citylist, image_num):
-    print 'calculating sizing parameters'
-    fig = plt.figure(figsize=(19, 9))
-    cosine_similarities = np.array(cosine_similarities)
-    sz = 20. * cosine_similarities / max(cosine_similarities) + 6
-    fs = np.zeros(len(cosine_similarities))
-    alp = np.zeros(len(cosine_similarities))
-    cs_sort = np.argsort(cosine_similarities)[::-1]
-
-    n1 = 4
-    n2 = 12
-    n3 = 40
-
-    alp[cs_sort[:n1]] = 1
-    alp[cs_sort[n1:n2]] = 0.0
-    alp[cs_sort[n2:n3]] = 0.40
-    alp[cs_sort[n3:]] = 0.2
-
-    fs[cs_sort[:n1]] = 13
-    fs[cs_sort[n1:n2]] = 10
-    fs[cs_sort[n2:n3]] = 6
-    fs[cs_sort[n3:]] = 4
-    print 'plotting the points'
-    alignment = ['left', 'right']
-    for i in xrange(len(xpts)):
-        plt.plot(xpts[i], ypts[i], '.', markersize=sz[i], color='#81D8D0', alpha=alp[i])
-        plt.text(xpts[i], ypts[i], citylist[i], fontsize=int(fs[i]), alpha=alp[i], color=[1, 1, 1], ha=alignment[i%2])
-
-    m.drawcoastlines(linewidth=0.2)
-    m.drawcountries(linewidth=0.2)
-    m.drawmapboundary(fill_color='#2B3856')
-    # fill continents, set lake color same as ocean color.
-    m.fillcontinents(color='#786D5F', lake_color='#2B3856')
-    image_filename = IMAGE_PATH.format(image_num)
-    plt.savefig(image_filename, bbox_inches='tight')
-    print 'plot saved'
-    return image_filename
-
-
-def get_city_image(cities):
-    return ['static/images/city_images_3/{}.png'.format(city) for city in cities]
+# load the basemap object and city coordinates for plotting
+m, coords = setup_app.load_map('../../data/base_map.pkl',
+                               '../../data/coordinates.txt')
 
 
 with open('../../data/personalities_R05.txt') as f:
@@ -114,48 +57,23 @@ def about():
 
 @app.route('/solve', methods=['GET', 'POST'])
 def solve():
-    user_data = request.json
-    selections = np.array(user_data.values())
-    categories = np.array(user_data.keys())
-    selected_categories = categories[selections]
-    imagenum = 0
-    for i, key in enumerate(sorted(user_data.keys())):
-        imagenum += (i + 1) * user_data[key]
+    user_data = request.json  # record user-selected personality trait(s)
 
-    pers_list = [personality_dict[sc] for sc in selected_categories]
-    personality_list = [item for sublist in pers_list for item in sublist]
+    # get list of personality words and vectorized personality
+    personality_vector, personality_list = setup_app.get_personality_vector(user_data, tfidf, personality_dict)
 
-    personality_vector = tfidf.transform(np.array([' '.join(set(personality_list)), ]))
-    cos_sims = cosine_similarity(X, personality_vector)
-    top4 = np.argsort(cos_sims[:, 0])[::-1][:4]
+    # calculate cosine similarity and get top four cities
+    cosine_similarities = cosine_similarity(X, personality_vector)
+    top4 = np.argsort(cosine_similarities[:, 0])[::-1][:4]
     top_cities = [cities[n] for n in top4]
 
-    image_paths = get_city_image(top_cities)
-    sentence = []
-    for i, num in enumerate(top4):
-        s_array = re.split('[.!?]', sent_bodies[num])
-        # r'(?<=\w)\.(?!\..)|!'
-        # s_array = sent_bodies[num].split('.')
+    # get representative text snippet for top cities
+    sentence = setup_app.top_city_sentence(top4, sent_bodies, personality_list)
+    # get map plot
+    image_filename = setup_app.get_map_image(cosine_similarities, cities, m, coords, user_data)
+    # get city image paths
+    image_paths = setup_app.get_city_image(top_cities)
 
-        s_match = 0
-        top_sentence = ''
-        stopwds = ['Rick,', 'Gyugi']
-        for s in s_array:
-            s = s.replace('read more ', '')
-            s = s.replace('More ', '')
-            slen = len(set(s.split()) & set(personality_list))
-            forbidden = len(set(s.split()) & set(stopwds))
-            if slen > s_match and forbidden == 0:
-                s_match = slen
-                top_sentence = s
-            elif slen >= s_match and len(top_sentence) > 4 and len(s) < len(top_sentence) and len(s) > 75 and 'Gyugyi' not in s.split():
-                s_match = slen
-                top_sentence = s
-        sentence.append('"{}"'.format(top_sentence.strip()))
-    print 'trying to plot'
-
-    image_filename = plot_personality_map(m, cos_sims[:, 0], coords[:, 0], coords[:, 1], cities, imagenum)
-    print image_filename
     return jsonify({'city_1': top_cities[0], 'city_2': top_cities[1],
                     'city_3': top_cities[2], 'city_4': top_cities[3],
                     'c1text': sentence[0], 'c2text': sentence[1],
---
+++ src/app/setup_app.py
@@ -0,0 +1,151 @@
+from __future__ import division
+import pickle
+import pandas as pd
+import numpy as np
+import re
+import os.path
+import matplotlib.pyplot as plt
+
+
+def load_dataframes(dataframes):
+    '''
+    INPUT: list of paths to picked dataframes
+    OUTPUT: dataframe(s)
+    take paths to pickled dataframes and return dataframes
+    '''
+    return (pd.read_pickle(df) for df in dataframes)
+
+
+def load_models(pickle_files):
+    '''
+    INPUT: list of pickled models
+    OUTPUT: unpickled models
+    take list of pickled models and return the models
+    '''
+    models = []
+    for p_model in pickle_files:
+        with open(p_model) as f:
+            models.append(pickle.load(f))
+    return tuple(models)
+
+
+def load_map(pickled_map, coordinates_file):
+    '''
+    INPUT: pickled basemap object, city coordinates file
+    OUTPUT: basemap object, basemap coordinates of cities
+    '''
+    # load the basemap object for plotting
+    with open('../../data/base_map.pkl') as f:
+        m = pickle.load(f)
+
+    # load the city coordinates
+    with open('../../data/coordinates.txt') as f:
+        coords = np.array([[float(coord) for coord in line.strip('\n').split('|')[1:]] for line in f])
+
+    return m, coords
+
+
+def plot_personality_map(m, cosine_similarities, xpts, ypts, citylist, image_num, image_path):
+    '''
+    INPUT: basemap, cosine similarites, city coordinates and names, image name and paths
+    OUTPUT: map image path
+    create map image by cosine similarity
+    '''
+    fig = plt.figure(figsize=(19, 9))  # initialize figure
+    # set markersize, font size and alpha by cosine similarity
+    sz = 20. * cosine_similarities / max(cosine_similarities) + 6
+    fs = 9. * cosine_similarities / max(cosine_similarities) + 4
+    alp = 0.5 * cosine_similarities / max(cosine_similarities)
+    cs_sort = np.argsort(cosine_similarities)[::-1]
+    alp[cs_sort[:4]] = 1
+    fs[cs_sort[:4]] = 13
+
+    top4cities = citylist[cs_sort[:4]]
+    top4x = xpts[cs_sort[:4]]
+    order = list(top4cities[np.argsort(top4x)])
+    alignment = ['left', 'right']
+    for i in xrange(len(xpts)):
+        plt.plot(xpts[i], ypts[i], '.', markersize=sz[i], color='#81D8D0', alpha=alp[i])
+        if citylist[i] in top4cities:
+            plt.text(xpts[i], ypts[i], citylist[i], fontsize=int(fs[i]), alpha=alp[i],
+                     color=[1, 1, 1], ha=alignment[order.index(citylist[i]) % 2])
+        else:
+            plt.text(xpts[i], ypts[i], citylist[i], fontsize=int(fs[i]), alpha=alp[i],
+                     color=[1, 1, 1], ha=alignment[i % 2])
+
+    # fill in countries and water
+    m.drawcoastlines(linewidth=0.2)
+    m.drawcountries(linewidth=0.2)
+    m.drawmapboundary(fill_color='#2B3856')
+    m.fillcontinents(color='#786D5F', lake_color='#2B3856')
+
+    image_filename = image_path.format(image_num)
+    plt.savefig(image_filename, bbox_inches='tight')
+    plt.close('all')
+    return image_filename
+
+
+def get_map_image(cos_sims, cities, m, coords, user_data):
+    '''
+    INPUT: cosine similiarites, cities list and coordinates, basemap images, user selections
+    OUTPUT: path to image
+    '''
+    imagenum = ''.join([str(val * 1) for val in user_data.values()])
+    if os.path.isfile('static/images/map_images/mapimg_{}.png'.format(imagenum)):
+        return 'static/images/map_images/mapimg_{}.png'.format(imagenum)
+    else:
+        return plot_personality_map(m, np.array(cos_sims[:, 0]), coords[:, 0], coords[:, 1],
+                                    cities, imagenum, 'static/images/mapimg_{}.png')
+    return image_file
+
+
+def get_personality_vector(user_data, tfidf, personality_dict):
+    '''
+    INPUT: user data, model, personality dictionary
+    OUTPUT: personality vector and personality list
+    '''
+    selections = np.array(user_data.values())
+    categories = np.array(user_data.keys())
+    selected_categories = categories[selections]
+
+    pers_list = [personality_dict[sc] for sc in selected_categories]
+    personality_list = [item for sublist in pers_list for item in sublist]
+
+    personality_vector = tfidf.transform(np.array([' '.join(set(personality_list)), ]))
+    return personality_vector, personality_list
+
+
+def top_city_sentence(top_inds, sent_bodies, personality_list):
+    '''
+    INPUT: city indices, text, personality list
+    OUTPUT: text snippets for top cities
+    '''
+    sentence = []
+    stopwds = ['Rick,', 'Gyugyi', 'killing']
+    for i, num in enumerate(top_inds):
+        s_array = re.split('[.!?]', sent_bodies[num])
+        s_match = 0
+        top_sentence = ''
+        print len(set(personality_list))
+        for s in s_array:
+            s = s.replace('read more ', '').replace('More ', '')
+            slen = len(set(s.split()) & set(personality_list))
+            out_count = len(set(s.split()) & set(stopwds))
+            if slen > s_match and out_count == 0:
+                s_match = slen
+                top_sentence = s
+            elif slen >= s_match and len(top_sentence) > 4 and len(s) < len(top_sentence) and \
+                    len(s) > 75 and out_count == 0:
+                s_match = slen
+                top_sentence = s
+        tops_sentence = s if len(top_sentence) == 0 else top_sentence
+        sentence.append('"{}"'.format(top_sentence.strip()))
+    return sentence
+
+
+def get_city_image(cities):
+    '''
+    INPUT: cities
+    OUTPUT: path to city images
+    '''
+    return ['static/images/city_images_4/{}.png'.format(city) for city in cities]
